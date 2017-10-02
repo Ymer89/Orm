e7f560909726d90117ef91bf6675e3f14e1a6f19
Joakim Hammarberg <joakim.hammarberg89@gmail.com>
2017-10-02 17:27:23 +0200

    Add project files.

---
+++ Orm/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Orm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
