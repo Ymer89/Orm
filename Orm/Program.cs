fd47daa35a4dc8c1a746e01b89b95ce9bb81e2cf
Joakim Hammarberg <joakim.hammarberg89@gmail.com>
2017-10-02 17:51:27 +0200

    En bit på vägen

--- Orm/Program.cs
+++ Orm/Program.cs
@@ -6,7 +6,21 @@ namespace Orm
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int xposition = 45;
+            int yposition = 25;
+
+            Console.SetCursorPosition(xposition, yposition);
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine((char)4);
+
+            ByggVägg();
+        }
+        private static void ByggVägg()
+        {
+            for (int i = 1; i < 41; i++)
+            {
+             Console.ForegroundColor = ConsoleColor
+            }
         }
     }
 }
